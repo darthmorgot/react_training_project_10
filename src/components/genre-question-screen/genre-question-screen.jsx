02e8ad0419998ce1b097a228f53eb551fe506cee
darthmorgot <dartmorgot@yandex.ru>
2020-11-15 21:12:28 +1000

    Добавил форму с вариантами ответов

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -24,7 +24,7 @@ class GenreQuestionScreen extends PureComponent {
             <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
           </a>
 
-          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <svg xmlns="http://www.w3.org/2000/svg" classNameName="timer" viewBox="0 0 780 780">
             <circle className="timer__line" cx="390" cy="390" r="370"
               style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
           </svg>
@@ -37,7 +37,37 @@ class GenreQuestionScreen extends PureComponent {
         </header>
 
         <section className="game__screen">
-          <h2 className="game__title">Выберите инди-рок треки</h2>
+          <h2 className="game__title">Выберите {genre} треки</h2>
+          <form className="game__tracks">
+            {answers.map((answer, i) => (
+              <div key={`${i}-${answer.src}`} className="track">
+                <button className="track__button track__button--play" type="button" />
+                <div className="track__status">
+                  <audio src={answer.src} />
+                </div>
+                <div className="game__answer">
+                  <input
+                    className="game__input visually-hidden"
+                    type="checkbox"
+                    name="answer"
+                    value={`answer-${i}`}
+                    id={`answer-${i}`}
+                    checked={userAnswers[i]}
+                    onChange={(evt) => {
+                      const value = evt.target.checked;
+
+                      this.setState({
+                        answers: [...userAnswers.slice(0, i), value, ...userAnswers.slice(i + 1)]
+                      });
+                    }}
+                  />
+                  <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
+                </div>
+              </div>
+            ))}
+
+            <button className="game__submit button" type="submit">Ответить</button>
+          </form>
         </section>
       </section>
     );
