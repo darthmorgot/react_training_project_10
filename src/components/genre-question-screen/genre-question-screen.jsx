acc6f20d129e45059921d159fd8134660fa561e6
darthmorgot <dartmorgot@yandex.ru>
2020-11-21 18:20:04 +1000

    Подключил компонент AudioPlayer

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {GameType} from '../../const';
+import AudioPlayer from '../audio-player/auidio-player';
 
 class GenreQuestionScreen extends PureComponent {
   constructor(props) {
@@ -47,10 +48,7 @@ class GenreQuestionScreen extends PureComponent {
           >
             {answers.map((answer, i) => (
               <div key={`${i}-${answer.src}`} className="track">
-                <button className="track__button track__button--play" type="button" />
-                <div className="track__status">
-                  <audio src={answer.src} />
-                </div>
+                <AudioPlayer isPlaying={i === 0} src={answer.src} />
                 <div className="game__answer">
                   <input
                     className="game__input visually-hidden"
