1c144d13aaee5723d41fca2c2cdc2d812777dc9e
darthmorgot <dartmorgot@yandex.ru>
2020-11-22 17:39:30 +1000

    Удалил AudioPlayer, добавил renderPlayer

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,21 +1,19 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {GameType} from '../../const';
-import AudioPlayer from '../audio-player/auidio-player';
 
 class GenreQuestionScreen extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      activePlayer: 0,
       answers: [false, false, false, false]
     };
   }
 
   render() {
-    const {onAnswer, question} = this.props;
-    const {answers: userAnswers, activePlayer} = this.state;
+    const {onAnswer, question, renderPlayer} = this.props;
+    const {answers: userAnswers} = this.state;
     const {answers, genre} = question;
 
     return (
@@ -49,13 +47,7 @@ class GenreQuestionScreen extends PureComponent {
           >
             {answers.map((answer, i) => (
               <div key={`${i}-${answer.src}`} className="track">
-                <AudioPlayer
-                  onPlayButtonClick={() => this.setState({
-                    activePlayer: activePlayer === i ? -1 : i
-                  })}
-                  isPlaying={i === activePlayer}
-                  src={answer.src}
-                />
+                {renderPlayer(answer.src, i)}
                 <div className="game__answer">
                   <input
                     className="game__input visually-hidden"
@@ -94,7 +86,8 @@ GenreQuestionScreen.propTypes = {
     })).isRequired,
     genre: PropTypes.string.isRequired,
     type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired
-  }).isRequired
+  }).isRequired,
+  renderPlayer: PropTypes.func.isRequired
 };
 
 export default GenreQuestionScreen;
