f8dce0971425d4e6457397076e19e68757b1c020
darthmorgot <dartmorgot@yandex.ru>
2020-11-22 17:28:04 +1000

    Удалил state, заменил AudioPlayer на renderPlayer

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,20 +1,10 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {GameType} from '../../const';
-import AudioPlayer from '../audio-player/auidio-player';
 
 class ArtistQuestionScreen extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isPlaying: true
-    };
-  }
-
   render() {
-    const {isPlaying} = this.state;
-    const {onAnswer, question} = this.props;
+    const {onAnswer, question, renderPlayer} = this.props;
     const {answers, song} = question;
 
     return (
@@ -41,11 +31,7 @@ class ArtistQuestionScreen extends PureComponent {
           <h2 className="game__title">Кто исполняет эту песню?</h2>
           <div className="game__track">
             <div className="track">
-              <AudioPlayer
-                isPlaying={true}
-                src={song.src}
-                onPlayButtonClick={() => this.state({isPlaying: !isPlaying})}
-              />
+              {renderPlayer(song.src, 0)}
             </div>
           </div>
 
@@ -88,7 +74,8 @@ ArtistQuestionScreen.propTypes = {
       src: PropTypes.string.isRequired
     }).isRequired,
     type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired
-  }).isRequired
+  }).isRequired,
+  renderPlayer: PropTypes.func.isRequired
 };
 
 export default ArtistQuestionScreen;
