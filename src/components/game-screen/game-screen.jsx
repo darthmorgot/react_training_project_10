7699d01dea42826250073832d9322cb1f44acee9
darthmorgot <dartmorgot@yandex.ru>
2020-11-28 16:51:38 +1000

    Подключил компонент Mistakes

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -6,6 +6,7 @@ import {ActionCreator} from '../../store/action';
 import {GameType} from '../../const';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import Mistakes from '../mistakes/mistakes';
 
 import withAudioPlayer from '../../hocs/with-audio-player/with-audio-player';
 
@@ -13,7 +14,7 @@ const GenreQuestionScreenWrapped = withAudioPlayer(GenreQuestionScreen);
 const ArtistQuestionScreenWrapped = withAudioPlayer(ArtistQuestionScreen);
 
 const GameScreen = (props) => {
-  const {questions, step, onUserAnswer, resetGame} = props;
+  const {questions, step, onUserAnswer, resetGame, mistakes} = props;
   const question = questions[step];
 
   if (step >= questions.length || !question) {
@@ -30,14 +31,18 @@ const GameScreen = (props) => {
         <ArtistQuestionScreenWrapped
           question={question}
           onAnswer={onUserAnswer}
-        />
+        >
+          <Mistakes count={mistakes}/>
+        </ArtistQuestionScreenWrapped>
       );
     case GameType.GENRE:
       return (
         <GenreQuestionScreenWrapped
           question={question}
           onAnswer={onUserAnswer}
-        />
+        >
+          <Mistakes count={mistakes}/>
+        </GenreQuestionScreenWrapped>
       );
   }
 
@@ -48,11 +53,13 @@ GameScreen.propTypes = {
   questions: PropTypes.array.isRequired,
   step: PropTypes.number.isRequired,
   resetGame: PropTypes.func.isRequired,
-  onUserAnswer: PropTypes.func.isRequired
+  onUserAnswer: PropTypes.func.isRequired,
+  mistakes: PropTypes.number.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  step: state.step
+  step: state.step,
+  mistakes: state.mistakes
 });
 
 const mapDispatchToProps = (dispatch) => ({
