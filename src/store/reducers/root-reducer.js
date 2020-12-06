f03cb47e260f252fb4a0a062c860c1554c69ac1d
darthmorgot <dartmorgot@yandex.ru>
2020-12-06 17:55:18 +1000

    Добавил новое пространство имен

--- src/store/reducers/root-reducer.js
+++ src/store/reducers/root-reducer.js
@@ -1,13 +1,16 @@
 import {combineReducers} from 'redux';
 import {gameProcess} from '../reducers/game-process/game-process';
 import {gameData} from '../reducers/game-data/game-data';
+import {user} from '../reducers/user/user';
 
 export const NameSpace = {
   DATA: `DATA`,
-  GAME: `GAME`
+  GAME: `GAME`,
+  USER: `USER`
 };
 
 export default combineReducers({
   [NameSpace.DATA]: gameData,
-  [NameSpace.GAME]: gameProcess
+  [NameSpace.GAME]: gameProcess,
+  [NameSpace.USER]: user
 });
