639894bed95fce820a26c7f5bef223c268dc6d61
darthmorgot <dartmorgot@yandex.ru>
2020-12-15 21:18:41 +1000

    Создал тест для artist-question-screen.jsx

---
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ArtistQuestionScreen from './artist-question-screen';
+
+const AVATAR_URL = `https://avatars.dicebear.com/api/avataaars`;
+
+const question = {
+  type: `artist`,
+  song: {
+    artist: `Jim Beam`,
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`
+  },
+  answers: [
+    {
+      picture: `${AVATAR_URL}/JohnSnow.svg`,
+      artist: `John Snow`
+    },
+    {
+      picture: `${AVATAR_URL}/JackDaniels.svg`,
+      artist: `Jack Daniels`
+    },
+    {
+      picture: `${AVATAR_URL}/JimBeam.svg`,
+      artist: `Jim Beam`
+    }
+  ]
+};
+
+const noop = () => {};
+
+it(`ArtistQuestionScreen is rendered correctly`, () => {
+  const tree = renderer
+    .create(
+        <ArtistQuestionScreen
+          question={question}
+          onAnswer={noop}
+          renderPlayer={noop}
+        >
+          <React.Fragment />
+        </ArtistQuestionScreen>
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshpt();
+});
