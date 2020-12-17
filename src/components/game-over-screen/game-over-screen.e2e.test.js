9c8c910339254531ac35fffdb893a07a637b49f5
darthmorgot <dartmorgot@yandex.ru>
2020-12-17 21:33:46 +1000

    Создал enzyme-тест для game-over-screen.jsx

---
+++ src/components/game-over-screen/game-over-screen.e2e.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {GameOverScreen} from './game-over-screen';
+
+Enzyme.confugure({
+  adapter: new Adapter()
+});
+
+it(`Should replay button be pressed`, () => {
+  const handleReplayButtonClick = jest.fn();
+  const handleResetAction = jest.fn();
+
+  const wrapper = shallow(
+      <GameOverScreen
+        resetGameAction={handleResetAction}
+        onReplayBattonClick={handleReplayButtonClick}
+      />
+  );
+
+  const replayButton = wrapper.find(`button.replay`);
+  replayButton.simulate(`click`);
+
+  expect(handleReplayButtonClick).toHaveBeenCalledTimes(1);
+  expect(handleResetAction).toHaveBeenCalledTimes(1);
+});
+
