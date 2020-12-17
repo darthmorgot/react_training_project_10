d30d309e001ebeedc8613e7d650f318515053313
darthmorgot <dartmorgot@yandex.ru>
2020-12-17 22:04:24 +1000

    Создал enzyme-тест для welcome-screen.jsx

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WelcomeScreen from './welcome-screen';
+
+Enzyme.confugure({
+  adapter: new Adapter()
+});
+
+it(`Should welcome button be pressed`, () => {
+  const handlePlayButtonClick = jest.fn();
+
+  const welcomeScreen = shallow(
+      <WelcomeScreen
+        errorsCount={3}
+        onPlayBattonClick={handlePlayButtonClick}
+      />
+  );
+
+  const welcomeButton = welcomeScreen.find(`button.welcome__button`);
+  welcomeButton.simulate(`click`);
+
+  expect(handlePlayButtonClick).toHaveBeenCalledTimes(1);
+});
