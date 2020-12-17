813bd92c60ebd63eb73cb6eef8c5323fd0e80f18
darthmorgot <dartmorgot@yandex.ru>
2020-12-17 22:10:13 +1000

    Создал enzyme-тест для win-screen.jsx

---
+++ src/components/win-screen/win-screen.e2e.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {WinScreen} from './win-screen';
+
+configure({adapter: new Adapter()});
+
+it(`Should replay button be pressed`, () => {
+  const handleReplayButtonClick = jest.fn();
+  const handleResetAction = jest.fn();
+
+  const wrapper = shallow(
+      <WinScreen
+        questionsCount={3}
+        mistakesCount={1}
+        resetGameAction={handleResetAction}
+        onReplayBattonClick={handleReplayButtonClick}
+      />
+  );
+
+  const replayButton = wrapper.find(`button.replay`);
+  replayButton.simulate(`click`);
+
+  expect(handleReplayButtonClick).toHaveBeenCalledTimes(1);
+  expect(handleResetAction).toHaveBeenCalledTimes(1);
+});
